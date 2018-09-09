b9bec5a7e15d6cddb30f45e001e006afdfbdd9f0
Emanuel Condo <emanuelcondo@gmail.com>
2018-09-09 02:08:37 -0300

    Environment setup

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const serveStatic = require('serve-static');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('ci with travis');
+app.use(serveStatic(__dirname + '/public'));
+
+app.get('/api/v1.0/', (req, res) => {
+  res.send('Welcome to Guarani Server');
 });
 
 app.listen(port, () => {
-  console.log('App running on port 3000');
+  console.log('App running on port ' + port);
 });
