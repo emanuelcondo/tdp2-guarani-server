ffca956c5972f0a542c59bc4db484886e3266467
Emanuel Condo <emanuelcondo@gmail.com>
2018-09-09 01:16:56 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('ci with travis');
+});
+
+app.listen(3000, () => {
+  console.log('App running on port 3000');
+});
---
+++ test/index.spec.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+
+describe('test', function() {
+    it('number 1 should be equal to number 1', function () {
+        assert.equal(1,1);
+    })
+});
