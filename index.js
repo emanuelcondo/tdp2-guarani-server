3683355a4d397d6b19e2a85f0ac9d6f6a3f00785
Emanuel Condo <emanuelcondo@gmail.com>
2018-09-09 01:49:55 -0300

    configurable port

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('ci with travis');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('App running on port 3000');
 });
