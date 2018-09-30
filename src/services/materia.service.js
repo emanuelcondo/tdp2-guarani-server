161cc95b38e9309f4ffd7202194ce3630b63bb4b
Emanuel Condo <emanuelcondo@gmail.com>
2018-09-30 01:22:02 -0300

    retrieve subjects with available courses

--- src/services/materia.service.js
+++ src/services/materia.service.js
@@ -1,20 +1,38 @@
 const Materia = require('../models/materia').Materia;
 const Carrera = require('../models/carrera').Carrera;
+const Curso = require('../models/curso');
 const logger = require('../utils/logger');
 const routes = require('../routes/routes');
 const HTTP = require('../utils/constants').HTTP;
+const async = require('async');
 
 module.exports.retrieveSubjectsByCarrer = (carrer_id, callback) => {
 
-    Carrera.findOne({ _id: carrer_id })
-            .populate('materias')
-            .exec((error, carrera) => {
-                if (error) {
-                    callback(error);
-                } else if (carrera) {
-                    callback(null, carrera.materias);
-                } else {
-                    callback(null, null);
-                }
-            });
+    async.waterfall([
+        (wCallback) => {
+            Carrera.findOne({ _id: carrer_id })
+                    .populate('materias')
+                    .exec(wCallback);
+        },
+        (carrer, wCallback) => {
+            if (carrer) {
+                let subject_ids = carrer.materias.map((item) => { return item._id; });
+                let query = { materia: { $in: subject_ids } }
+                Curso.findNoPopulate(query, (error, courses) => {
+                    wCallback(error, carrer.materias, courses);
+                });
+            } else {
+                wCallback(null, null);
+            }
+        },
+        (subjects, courses, wCallback) => {
+            if (subjects) {
+                let subjectsWithCourses = courses.map((item) => { return item.materia.toString(); });
+                let availableSubjects = subjects.filter((item) => { return subjectsWithCourses.indexOf(item._id.toString()) > -1; });
+                wCallback(null, availableSubjects);
+            } else {
+                wCallback(null, null);
+            }
+        }
+    ], callback);
 };
