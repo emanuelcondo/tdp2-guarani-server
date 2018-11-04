106b2064a65902d574258893d2aa279b8cad3d0e
Hugo Chavar <hechavar@gmail.com>
2018-11-04 08:13:56 -0300

    Changed cronTime to run every 30 minutes

--- src/cron-jobs/examen.cron.js
+++ src/cron-jobs/examen.cron.js
@@ -2,9 +2,9 @@ const CronJob = require('cron').CronJob;
 const ExamenService = require('../services/examen.service');
 const logger = require('../utils/logger');
 
-// At every 30th minute past every hour from 5 through 18
-// on every day-of-week from Sunday through Friday.
-const cronTime = '* */30 5-18 * * 0-5';
+// At every 30th minute past every hour
+// on every day-of-week from Sunday through Monday.
+const cronTime = '* */30 * * * *';
 const timeZone = 'America/Argentina/Buenos_Aires';
 
 new CronJob(cronTime, function() {
